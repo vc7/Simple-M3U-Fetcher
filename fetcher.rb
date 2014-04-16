43d12682c35fa3d7c02cc30820cbbbcc887e2e06
Vincent Chen <vince78718@gmail.com>
2014-04-16 23:07:11 +0800

    make the variable name more better

--- fetcher.rb
+++ fetcher.rb
@@ -1,17 +1,17 @@
-urlarray = []
+url_array = []
 
 File.new("m3u8.m3u", "r").each do
 	|line| 
 	if line =~ /^[http]/
 		
-		theurl = line.split(/[\?]/)[0]
+		url_string = line.split(/[\?]/)[0]
 		
-		if theurl != urlarray.last
-			urlarray.push(theurl)
+		if url_string != url_array.last
+			url_array.push(url_string)
 		end
 	end
 end
 
-urlarray.each_index do |index|
-	puts "lh-%.2i.flv | #{urlarray[index]}" %index
+url_array.each_index do |index|
+	puts "lh-%.2i.flv | #{url_array[index]}" %index
 end
